f9766799d3f8dca753b5d35df8eb0f0d2f1db4d3
Casper Korsgaard <casper.korsgaard@outlook.com>
2026-08-20 13:43:57 +0200

    update: Restore default database name in Aspire setup.

--- src/Casko.DefaultsForUmbraco.Aspire/AppHost.cs
+++ src/Casko.DefaultsForUmbraco.Aspire/AppHost.cs
@@ -14,8 +14,8 @@ if (sqlOnly)
     return;
 }
 
-//var databaseName = "defaults-for-umbraco-v3-db";
-var databaseName = "sst-local-database";
+var databaseName = "defaults-for-umbraco-v3-db";
+//var databaseName = "sst-local-database";
 var umbracoDb = sql
     .AddDatabase("umbracoDbDSN", databaseName)
     .WithCreationScript(GetUmbracoDatabaseCreationScript(databaseName));
